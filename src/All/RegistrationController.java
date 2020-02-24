1e52299815893b0c7c1d43870f57e552a15d553b
DanWillisUni <dan.willis.uni@gmail.com>
2020-02-24 14:30:56 +0000

    figured out how to make registration work,has errors

--- src/All/RegistrationController.java
+++ src/All/RegistrationController.java
@@ -2,17 +2,20 @@ package All;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 import java.sql.SQLException;
 
 public class RegistrationController {
     @FXML private Text actiontarget;
+    @FXML private TextField forename;
+    @FXML private TextField surname;
     @FXML
     protected void RegisterHandleSubmitButtonAction(ActionEvent event) throws SQLException, ClassNotFoundException {
         actiontarget.setText("Register button pressed");
         //get all the things and get a person from it
-        Person newPerson = new Person();
+        Person newPerson = new Person(forename.getText(),surname.getText());
         DatabaseController db = new DatabaseController("RegistrationController","@localhost","SoftwareEngineering","SoftwareEngineering");
         db.addPerson(newPerson);
     }
