87270a35a383561ad2c628ecff3bfe09148f31d5
DanWillisUni <dan.willis.uni@gmail.com>
2020-02-27 20:19:38 +0000

    fixed swimminging in sql file

--- SQL/adding exercises.sql
+++ SQL/adding exercises.sql
@@ -1,5 +1,5 @@
 insert into softwareengineering.exercise values(0,0,'other');
-insert into softwareengineering.exercise values(1,11,'swimminging');
+insert into softwareengineering.exercise values(1,11,'swimming');
 insert into softwareengineering.exercise values(2,12,'tennis');
 insert into softwareengineering.exercise values(5,15,'rowing');
 insert into softwareengineering.exercise values(3,13,'running');
