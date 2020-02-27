cc910e1e17196bbc4bf22184c8b7a3d3b33fb2b4
DanWillisUni <dan.willis.uni@gmail.com>
2020-02-27 10:42:48 +0000

    Added adding food into sql folder

---
+++ SQL/adding food.sql
@@ -0,0 +1,6 @@
+insert into softwareengineering.foods values(0,'apple',95,182);
+insert into softwareengineering.foods values(1,'banana',105,118);
+insert into softwareengineering.foods values(2,'orange',47,100);
+insert into softwareengineering.foods values(3,'strawberry',33,100);
+insert into softwareengineering.foods values(4,'grapes',67,100);
+insert into softwareengineering.foods values(5,'donut',402,100);
