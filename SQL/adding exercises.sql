0dd22833f61300ed1464016303d0772d988a874d
DanWillisUni <dan.willis.uni@gmail.com>
2020-02-27 13:08:15 +0000

    Added adding exercise into sql folder

---
+++ SQL/adding exercises.sql
@@ -0,0 +1,6 @@
+insert into softwareengineering.exercise values(0,10,'jogging');
+insert into softwareengineering.exercise values(1,11,'swimminging');
+insert into softwareengineering.exercise values(2,12,'tennis');
+insert into softwareengineering.exercise values(5,15,'rowing');
+insert into softwareengineering.exercise values(3,13,'running');
+insert into softwareengineering.exercise values(4,14,'skipping');
