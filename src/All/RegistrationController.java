b88691e520db45d0644c4ec9e7cc2bca00de4e26
DanWillisUni <dan.willis.uni@gmail.com>
2020-03-07 22:52:43 +0000

    validation on registration

--- src/All/RegistrationController.java
+++ src/All/RegistrationController.java
@@ -6,10 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.converter.BigDecimalStringConverter;
@@ -24,7 +21,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class RegistrationController {
-    @FXML private Text actiontarget;
     @FXML private TextField forename;
     @FXML private TextField surname;
     @FXML private TextField username;
@@ -34,8 +30,94 @@ public class RegistrationController {
     @FXML private DatePicker DOB;
     @FXML private ComboBox gender;
     @FXML private TextField height;
+    @FXML private Label errorMsg;
     @FXML
     protected void RegisterHandleSubmitButtonAction(ActionEvent event) throws IOException {
+        errorMsg.setText("");
+        if (forename.getText()!=null){
+            if (!forename.getText().equals("")){
+                if (forename.getText().toString().length()>20){
+                    errorMsg.setText("Error: forename too long");
+                }
+            }
+            errorMsg.setText("Error: forename null");
+        }
+        if (surname.getText()!=null){
+            if (!surname.getText().equals("")){
+                if (surname.getText().toString().length()>20){
+                    errorMsg.setText("Error: surname too long");
+                }
+            }
+            errorMsg.setText("Error: surname null");
+        }
+        if (username.getText()!=null){
+            if (!username.getText().equals("")){
+                if (username.getText().toString().length()>20){
+                    errorMsg.setText("Error: username too long");
+                }
+            }
+            errorMsg.setText("Error: username null");
+        }
+        if (email.getText()!=null){
+            if (!email.getText().equals("")){
+                if (email.getText().toString().length()<20){
+                    //regex if bothered
+                } else {
+                    errorMsg.setText("Error: email too long");
+                }
+            }
+            errorMsg.setText("Error: email null");
+        }
+        if (password.getText()!=null){
+            if (!password.getText().equals("")){
+                if (password.getText().toString().length()>20){
+                    errorMsg.setText("Error: password too long");
+                }
+            }
+            errorMsg.setText("Error: password null");
+        }
+        if (password2.getText()!=null){
+            if (!password2.getText().equals("")){
+                if (password2.getText().toString().length()>20){
+                    errorMsg.setText("Error: password2 too long");
+                }
+            }
+            errorMsg.setText("Error: password2 null");
+        }
+        if(DOB.getValue()!=null){
+            Long d = Date.from(Instant.from(DOB.getValue().atStartOfDay(ZoneId.systemDefault()))).getTime();
+            Long c = new Date().getTime();
+            if (c>d){
+                if (c - 3153600000000L > d){
+                    errorMsg.setText("Error: date to long ago");
+                }
+            } else {
+                errorMsg.setText("Error: date in the future");
+            }
+        }else {
+            errorMsg.setText("Error: Date not selected");
+        }
+        if (gender.getValue()==null) {
+            errorMsg.setText("Error: gender not selected");
+        }else if(gender.getValue().toString().equals("")){
+            errorMsg.setText("Error: gender not typed in");
+        } else {
+            if(!gender.getValue().toString().equals("Male")&&!gender.getValue().toString().equals("Female")&&!gender.getValue().toString().equals("Other")){
+                errorMsg.setText("Error: not valid gender");
+            }
+        }
+        if (height.getText().matches("^([1-9][0-9]*(\\.[0-9]+)?|0+\\.[0-9]*[1-9][0-9]*)$")){
+            int i = Integer.parseInt(height.getText());
+            if (i>0){
+                if (i>250){
+                    errorMsg.setText("Error: height greater than 250");
+                }
+            } else {
+                errorMsg.setText("Error: height negative");
+            }
+        } else {
+            errorMsg.setText("Error: height not numeric");
+        }
         //get all the things and get a person from it
         //check emails are unique
         //check email length less than 60
@@ -45,9 +127,7 @@ public class RegistrationController {
         //check height isnt crazy
         //check username length is less than 45
         //check username is individual
-        //check DOB isnt crazy
-        //check height is a number
-        if (password.getText().equals(password2.getText())){
+        if (password.getText().equals(password2.getText())&&errorMsg.getText().equals("")){
             Person newPerson = new Person(forename.getText(),surname.getText(),username.getText(),email.getText(),password.getText(), Date.from(Instant.from(DOB.getValue().atStartOfDay(ZoneId.systemDefault()))),new BigDecimal(height.getText()), gender.getValue().toString().charAt(0));
             DatabaseController db = new DatabaseController();
             db.addUser(newPerson);
@@ -57,7 +137,7 @@ public class RegistrationController {
             stage.setScene(scene);
             stage.show();
         } else {
-            actiontarget.setText("Passwords do not match");
+            errorMsg.setText("Passwords do not match");
         }
     }
     @FXML
