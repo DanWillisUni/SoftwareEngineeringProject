c0d8def8faa38cca48cec8b872bffd41866a509d
DanWillisUni <dan.willis.uni@gmail.com>
2020-03-07 22:32:15 +0000

    validation on add goal

--- src/All/AddGoalController.java
+++ src/All/AddGoalController.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -19,6 +20,7 @@ public class AddGoalController {
     private Person User;
     @FXML private TextField TargetWeight;
     @FXML private DatePicker targetDate;
+    @FXML private Label errorMsg;
     public void setUser(Person User){
         this.User = User;
     }
@@ -38,8 +40,36 @@ public class AddGoalController {
     }
     @FXML
     private void AddWeightGoalButtonAction (ActionEvent event) throws IOException {
-        DatabaseController db = new DatabaseController();
-        db.addGoal(User.getID(),Integer.parseInt(TargetWeight.getText()), Date.from(Instant.from(targetDate.getValue().atStartOfDay(ZoneId.systemDefault()))));
-        GoToDashButtonAction(event);
+        errorMsg.setText("");
+        if (TargetWeight.getText().matches("^([1-9][0-9]*(\\.[0-9]+)?|0+\\.[0-9]*[1-9][0-9]*)$")){
+            int i = Integer.parseInt(TargetWeight.getText());
+            if (i>0){
+                if (i>250){
+                    errorMsg.setText("Error: target greater than 250");
+                }
+            } else {
+                errorMsg.setText("Error: target negative");
+            }
+        } else {
+            errorMsg.setText("Error: target not numeric");
+        }
+        if(targetDate.getValue()!=null){
+            Long d = Date.from(Instant.from(targetDate.getValue().atStartOfDay(ZoneId.systemDefault()))).getTime();
+            Long c = new Date().getTime();
+            if (c<=d){
+                if (c + 31536000000L < d){
+                    errorMsg.setText("Error: target to far");
+                }
+            } else {
+                errorMsg.setText("Error: target date in the past");
+            }
+        }else {
+            errorMsg.setText("Error: Date not selected");
+        }
+        if (errorMsg.getText().equals("")){
+            DatabaseController db = new DatabaseController();
+            db.addGoal(User.getID(),Integer.parseInt(TargetWeight.getText()), Date.from(Instant.from(targetDate.getValue().atStartOfDay(ZoneId.systemDefault()))));
+            GoToDashButtonAction(event);
+        }
     }
 }
